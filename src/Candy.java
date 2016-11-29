a76478c88f3acea798308bbf04bf608ca4e5afe4
tatad6701 <tatad6701@HRH-IC0027335.wrdsb.ca>
2016-11-29 13:17:49 -0500

    Checking the assignment out

--- src/Candy.java
+++ src/Candy.java
@@ -1,23 +1,18 @@
+
 /**
- * The Candy class is a subclass of DessertItem. 
- * 
- * A Candy has a name, a weight (in pounds), and a price per pound. 
- * The weight and price per pound are used to calculate the cost of the Candy. 
- * 
+ * The Candy class is a subclass of DessertItem. This is an important class!
+ *
+ * A Candy has a name, a weight (in pounds), and a price per pound. The weight
+ * and price per pound are used to calculate the cost of the Candy.
+ *
  * @author YOURNAMEHERE
  */
 public class Candy {
-    
 
-    public Candy(String name, double weight, int pricePerLbs)
-    {
-
+    public Candy(String name, double weight, int pricePerLbs) {
     }
 
- 
-    
-    public String toString()
-    {
-       return "";
+    public String toString() {
+        return "";
     }
 }
