a3509346f1a66452deb3b4cb2db2dbf7414e772e
Denis <Denis@192.168.0.16>
2016-11-29 19:21:20 -0500

    Beginning to work on the Candy subclass (Commit 2)

--- src/Candy.java
+++ src/Candy.java
@@ -5,11 +5,26 @@
  * A Candy has a name, a weight (in pounds), and a price per pound. The weight
  * and price per pound are used to calculate the cost of the Candy.
  *
- * @author YOURNAMEHERE
+ * @author @tatad6701
  */
-public class Candy {
+public class Candy extends DessertItem {
 
+    // Create instance variables to be used
+    private String name;
+    private double weight;
+    private int pricePerlibs;
+
+    /**
+     * Constructor for the Candy
+     *
+     * @param name the name of the candy being passed through
+     * @param weight the weight of the candy being passed through
+     * @param pricePerLbs the price per pounds of the candy being passed through
+     */
     public Candy(String name, double weight, int pricePerLbs) {
+        super(name);
+        this.weight = weight;
+        this.pricePerlibs = pricePerLbs;
     }
 
     public String toString() {
--- src/IceCream.java
+++ src/IceCream.java
@@ -1,21 +1,19 @@
+
 /**
- * The IceCream class is a subclass of DessertItem. 
- * 
+ * The IceCream class is a subclass of DessertItem.
+ *
  * IceCream has a name, and a cost associated with it.
- * 
- * @author YOURNAMEHERE
+ *
+ * @author @tatad6701
  */
 public class IceCream {
 
-    public IceCream(String name, int cost)
-    {
-        
+    public IceCream(String name, int cost) {
+
     }
 
-  
-    public String toString()
-    {
-     return "";
+    public String toString() {
+        return "";
     }
-    
+
 }
