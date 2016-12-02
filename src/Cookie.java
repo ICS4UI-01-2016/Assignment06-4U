47c1d9ee33c8fe2290f5d42e0dc661f071fe76a8
halll7908 <halll7908@HRH-IC0037381.wrdsb.ca>
2016-12-02 11:18:50 -0500

    Progress on Assignment 6.

--- src/Cookie.java
+++ src/Cookie.java
@@ -1,24 +1,31 @@
+
 /**
- * The Cookie class is a subclass of DessertItem. 
- * 
- * A Cookie has a name, the number of cookies purchased, and 
- * the price per dozen cookies. 
- * The number and price per dozen are used to calculate the cost of the cookies.
- * 
+ * The Cookie class is a subclass of DessertItem.
+ *
+ * A Cookie has a name, the number of cookies purchased, and the price per dozen
+ * cookies. The number and price per dozen are used to calculate the cost of the
+ * cookies.
+ *
  * @author YOURNAMEHERE
  */
-public class Cookie {
-    
-    
-    public Cookie(String name, int number, int pricePer12)
-    {
-        
+public class Cookie extends DessertItem {
+
+    public Cookie(String name, int number, int pricePer12) {
+        // call to dessertitem constructor
+        super(name, number, pricePer12);
     }
 
-    
-    public String toString()
-    {
-        return "";
+    @Override
+    public String toString() {
+        String output = this.getNumber() + " @ $" + pricePer12 + "/dz.";
+        output = this.getName() + TAB + this.getCost();                         // how to do a tab
+        return output;
     }
-    
+
+    @Override
+    public void cost() {
+        int perOne = pricePer12/12;
+        return perOne * this.getNumber();
+        }
+    }
 }
--- src/DessertItem.java
+++ src/DessertItem.java
@@ -1,49 +1,58 @@
 
 /**
  * Abstract superclass for Dessert Item hierarchy
- * 
- * This class represents a generic dessert item for the inheritance/polymorphism assignment.
- * All other dessert items will be inheriting from this class.
- * DO NOT CHANGE THIS CLASS!
- * 
+ *
+ * This class represents a generic dessert item for the inheritance/polymorphism
+ * assignment. All other dessert items will be inheriting from this class. DO
+ * NOT CHANGE THIS CLASS!
+ *
  * @author lamonta
- */ 
-public abstract class DessertItem {
-  
-  private String name;
-  
-/**
- * Null constructor for DessertItem class
  */
-  public DessertItem() {
-    this("");
-  }
-  
-/**
- * Initializes DessertItem data
- * @param name The name of the dessert item being created.
- */   
-  public DessertItem(String name) {
-   this.name = name;
-  }
-  
-/**
- * Returns name of DessertItem
- * @return name of DessertItem
- */  
-  public final String getName() {
-    return name;
-  }
-  
-/**
- * Returns cost of DessertItem
- * @return cost of DessertItem
- */  
-  public abstract int getCost();
+public abstract class DessertItem {
 
+    private String name;
+    private int number;
+    private int cost;
+
+    /**
+     * Null constructor for DessertItem class
+     */
+    public DessertItem() {
+        this.name = name;
+        this.number = number;
+        this.cost = cost;
+    }
+
+    /**
+     * Initializes DessertItem data
+     *
+     * @param name The name of the dessert item being created.
+     */
+    public DessertItem(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns name of DessertItem
+     *
+     * @return name of DessertItem
+     */
+    public final String getName() {                       // what does this do?
+        return this.name;
+    }
+
+    /**
+     * Returns number of items
+     * @return number of items
+     */
+    public int getNumber(){
+        return this.number;
+    }
+    
+    /**
+     * Returns cost of DessertItem
+     *
+     * @return cost of DessertItem
+     */
+    public abstract int getCost();
 }
-   
-  
-  
-   
-   
