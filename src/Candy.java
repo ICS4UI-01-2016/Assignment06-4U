d120b9bced016ffe141d604cfdc238a59f9b3367
Nick Joudrey <Nick Joudrey@Nicks_PC>
2016-12-03 16:19:02 -0500

    :)

--- src/Candy.java
+++ src/Candy.java
@@ -8,30 +8,51 @@
  * @author YOURNAMEHERE
  */
 public class Candy extends DessertItem {
-
+    //instance variables
     private double weight;
     private int pricePerLbs;
 
+    /**
+     * constructor method for Candy
+     * @param name name of candy
+     * @param weight weight of candy
+     * @param pricePerLbs price per pound of candy
+     */
     public Candy(String name, double weight, int pricePerLbs) {
+        //pass in name, weight, and price per pound
         super(name);
         this.weight = weight;
         this.pricePerLbs = pricePerLbs;
     }
-
+    
+    /**
+     * accessor method for the cost of the candy
+     * @return cost of candy
+     */
     @Override
     public int getCost() {
+        //return the weight multipled by the price per pound as an int
         return (int) (weight * pricePerLbs);
     }
-
+    
+    /**
+     * toString method to print out a candies parameters
+     *
+     * @return the created candies parameters as a string
+     */
     @Override
     public String toString() {
+        //width of recipt in spaces
         int width = DessertShoppe.RECEIPT_WIDTH;
+        //the cost as a string 
         String cost = DessertShoppe.cents2dollarsAndCents(this.getCost());
+        //the remaining spaces after subtracting the length of the name of the candy
         int rSpaces = width - super.getName().length();
-        
+        //creating an output string with the same format as the example recipt
         String output = this.weight + " @ $" + DessertShoppe.cents2dollarsAndCents(pricePerLbs) + " /lb. \n" + super.getName();
-        
+        //format the space between the calculated price with the spaces and add it to the output string
         output += String.format("%" + rSpaces + "s%n", cost);
+        //return output
         return output;
     }
 }
--- src/Cookie.java
+++ src/Cookie.java
@@ -10,30 +10,55 @@
  */
 public class Cookie extends DessertItem {
 
+    //instance variables
     private int number;
     private int pricePer12;
 
+    /**
+     * constructor method for cookie
+     *
+     * @param name the name of cookie
+     * @param number the number of cookies
+     * @param pricePer12 the price per 12 cookies
+     */
     public Cookie(String name, int number, int pricePer12) {
+        //pass in name, number and price per 12
         super(name);
         this.number = number;
         this.pricePer12 = pricePer12;
     }
 
+    /**
+     * accessor method to get the cost of the cookies
+     *
+     * @return return the cost
+     */
     @Override
     public int getCost() {
-        int price = (int) Math.round((number / 12.0) * pricePer12);
+        //calculate the price as an int and round
+        int price = (int) Math.round((this.number / 12.0) * this.pricePer12);
+        //return price
         return price;
     }
 
+    /**
+     * toString method to print out a cookies parameters
+     *
+     * @return the created cookies parameters as a string
+     */
     @Override
     public String toString() {
+        //width of recipt in spaces
         int width = DessertShoppe.RECEIPT_WIDTH;
+        //the cost as a string 
         String cost = DessertShoppe.cents2dollarsAndCents(this.getCost());
+        //the remaining spaces after subtracting the length of the name of the cookie
         int rSpaces = width - super.getName().length();
-        
+        //creating an output string with the same format as the example recipt
         String output = this.number + " @ $" + DessertShoppe.cents2dollarsAndCents(pricePer12) + " /dz \n" + super.getName();
-        
+        //format the space between the calculated price with the spaces and add it to the output string
         output += String.format("%" + rSpaces + "s%n", cost);
+        //return output
         return output;
     }
 
