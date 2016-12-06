80776525adc09f2bebc6792b7b846366fcdeba43
fostp4040 <fostp4040@HRH-IC0027333.wrdsb.ca>
2016-12-06 13:28:29 -0500

    made ice cream

--- src/IceCream.java
+++ src/IceCream.java
@@ -3,19 +3,36 @@
  * 
  * IceCream has a name, and a cost associated with it.
  * 
- * @author YOURNAMEHERE
- */
-public class IceCream {
+ * @author fostp4040
+ */ 
+public class IceCream extends DessertItem {
 
+    private int cost;
     public IceCream(String name, int cost)
     {
-        
+        super(name);
+        this.cost=cost;
+    }
+    public int cost(){
+        return this.cost;
     }
 
-  
+    @Override
     public String toString()
     {
-     return "";
+        int width = DessertShoppe.RECEIPT_WIDTH;
+        width=width-super.getName().length()-4;
+        String space = " ";
+        for(int i =0; i<width; i++){
+            space=" ";
+        }
+        String output = super.getName()+space+DessertShoppe.cents2dollarsAndCents(this.getCost());
+        return output;
+    }
+
+    @Override
+    public int getCost() {
+        return cost;
     }
     
 }
