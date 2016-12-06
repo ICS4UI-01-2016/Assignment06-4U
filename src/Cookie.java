9ffb7e74bdbe2740816ba6b7e13e0111b42ca3b5
fostp4040 <fostp4040@HRH-IC0027333.wrdsb.ca>
2016-12-06 13:28:10 -0500

    made cookies

--- src/Cookie.java
+++ src/Cookie.java
@@ -5,20 +5,41 @@
  * the price per dozen cookies. 
  * The number and price per dozen are used to calculate the cost of the cookies.
  * 
- * @author YOURNAMEHERE
- */
-public class Cookie {
+ * @author fostp4040
+ */ 
+public class Cookie extends DessertItem{
     
-    
+    private int number;
+    private double pricePer12;
     public Cookie(String name, int number, int pricePer12)
     {
-        
+        super(name);
+        this.pricePer12=pricePer12;
+        this.number=number;
     }
 
+    public int number(){
+        return this.number;
+    }
+    public double pricePer12(){
+        return this.pricePer12;
+    }
     
+    @Override
     public String toString()
     {
-        return "";
+        int width = DessertShoppe.RECEIPT_WIDTH;
+        width=width-super.getName().length()-4;
+        String space = null;
+        for(int i =0; i<width; i++){
+            space=" ";
+        }
+        String output = this.number+ " @ $"+ this.pricePer12*0.01+"/dz."+"\n"+ super.getName()+space+DessertShoppe.cents2dollarsAndCents(this.getCost());
+        return output;
+    }
+    @Override
+    public int getCost() {
+        return (int) Math.round(number*pricePer12/12);
     }
     
 }
