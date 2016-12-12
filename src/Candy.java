672c3905b3f2e52b060f714e5caf0c7eabaa9a4a
moraj0721 <moraj0721@HRH-IC0027327.wrdsb.ca>
2016-12-12 13:59:45 -0500

    Commented and finished

--- src/Candy.java
+++ src/Candy.java
@@ -1,23 +1,54 @@
+
 /**
- * The Candy class is a subclass of DessertItem. 
- * 
- * A Candy has a name, a weight (in pounds), and a price per pound. 
- * The weight and price per pound are used to calculate the cost of the Candy. 
- * 
+ * The Candy class is a subclass of DessertItem.
+ *
+ * A Candy has a name, a weight (in pounds), and a price per pound. The weight
+ * and price per pound are used to calculate the cost of the Candy.
+ *
  * @author YOURNAMEHERE
  */
-public class Candy {
-    
+public class Candy extends DessertItem {
 
-    public Candy(String name, double weight, int pricePerLbs)
-    {
+    //weight of the candy
+    private double weight;
+    //price per pound
+    private int pricePerLbs;
 
+    public Candy(String name, double weight, int pricePerLbs) {
+        //call the super class
+        super(name);
+        this.pricePerLbs = pricePerLbs;
+        this.weight = weight;
     }
 
- 
-    
-    public String toString()
-    {
-       return "";
+    /**
+     * Gets the cost of the candy
+     *
+     * @return the price of the candy
+     */
+    @Override
+    public int getCost() {
+        return (int) Math.round(weight * pricePerLbs);
+    }
+
+    /**
+     * Prints the names and prices of candy
+     *
+     * @return spaces
+     */
+    @Override
+    public String toString() {
+        //converts the cent amount to dollars
+        String length = DessertShoppe.cents2dollarsAndCents(this.getCost());
+        //get how many spaces between name and price
+        int width = DessertShoppe.RECEIPT_WIDTH - super.getName().length() - length.length();
+        //constructing the order that things are printed
+        String output = this.weight + "lbs. @ " + DessertShoppe.cents2dollarsAndCents(this.pricePerLbs) + " /lb. \n" + super.getName();
+        //prints the spaces
+        for (int i = 0; i < width; i++) {
+            output += " ";
+        }
+        output += length;
+        return output;
     }
 }
--- src/Cookie.java
+++ src/Cookie.java
@@ -1,24 +1,55 @@
+
 /**
- * The Cookie class is a subclass of DessertItem. 
- * 
- * A Cookie has a name, the number of cookies purchased, and 
- * the price per dozen cookies. 
- * The number and price per dozen are used to calculate the cost of the cookies.
- * 
+ * The Cookie class is a subclass of DessertItem.
+ *
+ * A Cookie has a name, the number of cookies purchased, and the price per dozen
+ * cookies. The number and price per dozen are used to calculate the cost of the
+ * cookies.
+ *
  * @author YOURNAMEHERE
  */
-public class Cookie {
-    
-    
-    public Cookie(String name, int number, int pricePer12)
-    {
-        
+public class Cookie extends DessertItem {
+    //number of dozens
+
+    private int num;
+    //price per dozen cookies
+    private int pricePer12;
+
+    public Cookie(String name, int number, int pricePer12) {
+        //calls the super class for the name
+        super(name);
+        this.num = number;
+        this.pricePer12 = pricePer12;
     }
 
-    
-    public String toString()
-    {
-        return "";
+    /**
+     * Gets the overall cost and rounds it
+     *
+     * @return
+     */
+    @Override
+    public int getCost() {
+        return (int) Math.round(pricePer12 * num / 12);
     }
-    
+
+    /**
+     * Prints the name and prices of cookies, spaces between name and price
+     *
+     * @return name and prices of cookies, spaces
+     */
+    @Override
+    public String toString() {
+        //converts the cent amount to dollars
+        String length = DessertShoppe.cents2dollarsAndCents(this.getCost());
+        //gets spaces between name and price
+        int width = DessertShoppe.RECEIPT_WIDTH - super.getName().length() - length.length();
+        //constructing the order that things are printed
+        String output = this.num + " @ " + DessertShoppe.cents2dollarsAndCents(this.pricePer12) + " /dz. \n" + super.getName();
+        //prints the spaces
+        for (int i = 0; i < width; i++) {
+            output += " ";
+        }
+        output += length;
+        return output;
+    }
 }
