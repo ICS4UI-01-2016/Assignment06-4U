ca6350ccedf142eac4da9b580bf64c97cd871665
malcr1272 <malcr1272@HRH-IC0027298.wrdsb.ca>
2017-01-26 12:15:24 -0500

    comments

--- src/Sundae.java
+++ src/Sundae.java
@@ -11,20 +11,42 @@
  * 
  * @author YOURNAMEHERE
  */
-public class Sundae{
+public class Sundae extends IceCream{
+    //create variables
+    String toppingName;
+    int toppingCost;
     
-
-    
-    public Sundae(String icName, int icCost, String toppingName, int toppingCost)
-    {
-       
+    public Sundae(String icName, int icCost, String toppingName, int toppingCost){
+        //access ice cream name and cost
+    super(icName,icCost); 
+    //initialize variables
+    this.toppingName = toppingName;
+    this.toppingCost = toppingCost;
+    }
+        
+   /**
+   *get print for receipts
+   * @return ouput
+   */
+    @Override
+    public String toString(){ 
+   //make string for spaces
+    String spaces = new String();
+    //necessary amount of spaces
+    int run = 30 - (super.getName().length() + DessertShoppe.cents2dollarsAndCents(getCost()).length());
+    //add the amount of spaces
+    for(int i = 0; i < run; i++){
+    spaces = spaces + " ";
+    }
+    return (this.toppingName + " sundae" + " with\n" + super.getName() + spaces + DessertShoppe.cents2dollarsAndCents(this.getCost()));
     }
     
-
-    
-    public String toString()
-    {
-        return "";
-    }
-    
+    /**
+     * get cost
+     * @return cost
+     */
+    @Override
+    public int getCost(){
+     return super.getCost() + toppingCost;
+    } 
 }
