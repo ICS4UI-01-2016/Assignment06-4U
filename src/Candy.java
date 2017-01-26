f02240a6950f9a184b9f97aa9285f0812c8257ed
kulla6503 <kulla6503@HRH-IC0027300.wrdsb.ca>
2017-01-26 12:01:16 -0500

    Assignment 6 **Done**

--- src/Candy.java
+++ src/Candy.java
@@ -1,23 +1,98 @@
-/**
- * The Candy class is a subclass of DessertItem. 
- * 
- * A Candy has a name, a weight (in pounds), and a price per pound. 
- * The weight and price per pound are used to calculate the cost of the Candy. 
- * 
- * @author YOURNAMEHERE
- */
-public class Candy {
-    
 
-    public Candy(String name, double weight, int pricePerLbs)
-    {
+/**
+ * The Candy class is a subclass of DessertItem.
+ *
+ * A Candy has a name, a weight (in pounds), and a price per pound. The weight
+ * and price per pound are used to calculate the cost of the Candy.
+ *
+ * @author kulla6503
+ */
+public class Candy extends DessertItem {
+
+    // Creating a instance variable to store the weight 
+    private int pricePerLbs;
+    // Creating a instance variable to store the price per pound of candy
+    private double weight;
+
+    /**
+     * Creating a constructor which initializes the instance variables
+     *
+     * @param name of candy
+     * @param weight of candy
+     * @param pricePerLbs of candy
+     */
+    public Candy(String name, double weight, int pricePerLbs) {
+        
+        // The super class name is taken 
+        super(name);
+        
+        // Initializing the total weight of the candy 
+        this.weight = weight;
+        
+        // Initializing the price per pound of candy 
+        this.pricePerLbs = pricePerLbs;
+    }
+
+    /**
+     * Gets the weight of the candy
+     *
+     * @return the weight of the candy
+     */
+    public double getWeight() {
+        return this.weight;
+    }
+
+    /**
+     * Gets the price of candy per pound
+     *
+     * @return the price of candy per pound
+     */
+    private int getPricePerLbs() {
+        return this.pricePerLbs;
+    }
+
+    /**
+     * The string contains information of the candy
+     *
+     * @return the name, price per pound, weight, and total cost of the candy
+     */
+    @Override
+    public String toString() {
+
+        // The currency of cents is converted to dollars 
+        String output = DessertShoppe.cents2dollarsAndCents(this.getCost());
+
+        // The cost of candy is converted to a string 
+        String costLength = "" + this.getCost();
+
+        // Calculates the number of spaces between the cost of candy and the item name 
+        int numberofspaces = 30 - super.getName().length() - costLength.length() - 1;
+
+        // For loop created and used to print out the exact number of calculated spaces
+        for (int i = 0; i < numberofspaces; i++) {
+            output = " " + output;
+        }
+
+        // Returns the weight, price per pound, name, and total cost of candy 
+        return this.weight + " lbs. @ $" + this.pricePerLbs * 0.01 + " /lb. \n" + super.getName() + output;
 
     }
 
- 
-    
-    public String toString()
-    {
-       return "";
+    /**
+     * Method used to calculate cost of the candy
+     *
+     * @return the total cost of the candy
+     */
+    @Override
+    public int getCost() {
+        
+        // A formula used to calculate the total cost by multiplying the price per pound of candy with the weight of the candy
+        double cost = this.weight * this.pricePerLbs;
+
+        // Rounds the total cost of the candy 
+        double roundedCostOfCandy = Math.round(cost);
+
+        // Returns the total cost of the candy which is rounded 
+        return (int) roundedCostOfCandy;
     }
 }
--- src/Sundae.java
+++ src/Sundae.java
@@ -1,30 +1,95 @@
+
 /**
- * A Sundae is a type of IceCream. 
- * 
- * A Sundae needs the name of the ice cream being used, 
- * the cost of that ice cream, the name of the topping, and 
- * the cost for the topping. 
- * The cost is determined by adding the cost of the ice cream with 
- * the price of the topping.
- * 
+ * A Sundae is a type of IceCream.
+ *
+ * A Sundae needs the name of the ice cream being used, the cost of that ice
+ * cream, the name of the topping, and the cost for the topping. The cost is
+ * determined by adding the cost of the ice cream with the price of the topping.
+ *
  * Remember, we can use IceCream to do some of the heavy lifting for us!
- * 
- * @author YOURNAMEHERE
+ *
+ * @author kulla6503
  */
-public class Sundae{
-    
+public class Sundae extends IceCream {
 
+    // Creating a instance variable to store the the topping name 
+    private String toppingName;
     
-    public Sundae(String icName, int icCost, String toppingName, int toppingCost)
-    {
-       
-    }
-    
+    // Creating a instance variable to store the the topping cost 
+    private int toppingCost;
 
-    
-    public String toString()
-    {
-        return "";
+    /**
+     * Creating a constructor to initialize the instance variables
+     *
+     * @param name of the topping
+     * @param cost of the topping
+     */
+    public Sundae(String icName, int icCost, String toppingName, int toppingCost) {
+
+        // Super class name is taken, which is ice cream name and cost 
+        super(icName, icCost);
+
+        // Initializes the name of the topping 
+        this.toppingName = toppingName;
+        
+        // Initializes the cost of the topping 
+        this.toppingCost = toppingCost;
     }
-    
+
+    /**
+     * Gets the name of the topping
+     *
+     * @return the cost of the topping
+     */
+    public String getToppingName() {
+        return this.toppingName;
+    }
+
+    /**
+     * Gets the cost of the topping
+     *
+     * @return the cost of the topping
+     */
+    public int getToppingCost() {
+        return this.toppingCost;
+    }
+
+    /**
+     * Method used to calculate total cost of the sundae
+     *
+     * @return total cost of the sundae
+     */
+    @Override
+    public int getCost() {
+
+        // The cost of ice cream and topping is added 
+        int cost = super.getCost() + this.toppingCost;
+        return cost;
+    }
+
+    /**
+     * The string contains information of the Sundae
+     *
+     * @return the name of topping, name of ice cream, and the total cost
+     */
+    @Override
+    public String toString() {
+
+        // The currency of cents is converted to dollars 
+        String output = DessertShoppe.cents2dollarsAndCents(this.getCost());
+
+        // The cost of sundae is converted to a string 
+        String costLength = "" + this.getCost();
+
+        // Calculates the number of spaces between the cost of the sundae and the sundae name
+        int numberofspaces = 30 - super.getName().length() - costLength.length() - 1;
+
+        // For loop created and used to print out the exact number of calculated spaces
+        for (int i = 0; i < numberofspaces; i++) {
+            output = " " + output;
+        }
+
+        // Returns the name of the topping, the name and cost of ice cream
+        return this.toppingName + " Sundae with \n" + super.getName() + output;
+    }
 }
